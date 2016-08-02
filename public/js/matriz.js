3073404d99aac267b5b5d077b662c5d64da4b755
Pablo Díaz <pablo5_diaz@hotmail.com>
2016-08-01 23:35:54 -0600

    Lógica de ganador

--- public/js/matriz.js
+++ public/js/matriz.js
@@ -11,31 +11,77 @@ Matriz.prototype.verificarGanador = function() {
 	var contador = 0;
 	var last = -1;
 	//verificar ganador
+	//Primera pasada para verificar horizontal
 	for (i = 0; i < this.matrix.length ; i++) {
 		for (j = 0; j < this.matrix[i].length; j++){
 			
 			var tipo = this.matrix[i][j];
 			if (tipo != -1){
-				console.log('entra');
-				console.log(tipo);
-				console.log(tipoVerificador);
-				if ((i == last || j == last) && tipo == tipoVerificador) {
+				if (tipo == tipoVerificador || tipoVerificador == '') {
 					contador++;
+					tipoVerificador = tipo;
+				}
+				else {
+					tipoVerificador = 'not';
 				}
 				if (contador == 3){
 					alert('gano');
 					contador = 0;
 				}
-				if (last != i) {
-					last = i;
-				}
-				tipoVerificador = tipo;
+				
 			}
-
-			//console.log(this.matrix[i][j]);
 		}
+		contador = 0;
+		tipoVerificador = '';
 	}
-	console.log(this.matrix);
+	//segunda pasada para verificar verticales
+	for (i = 0; i < this.matrix.length ; i++) {
+		for (j = 0; j < this.matrix[i].length; j++){
+			
+			var tipo = this.matrix[j][i];
+			if (tipo != -1){
+				if (tipo == tipoVerificador || tipoVerificador == '') {
+					contador++;
+					tipoVerificador = tipo;
+				}
+				else {
+					tipoVerificador = 'not';
+				}
+				if (contador == 3){
+					alert('gano');
+					contador = 0;
+				}	
+			}
+		}
+		contador = 0;
+		tipoVerificador = '';
+	}
+	//tercero: verficar diagonales
+
+	if ((this.matrix[0][0] == 'equis' &&
+		this.matrix[1][1] == 'equis' &&
+		this.matrix[2][2] == 'equis')||
+		this.matrix[0][0] == 'circulo' &&
+		this.matrix[1][1] == 'circulo' &&
+		this.matrix[2][2] == 'circulo'
+		)
+	{
+		alert('gano');
+	}
+
+	if ((this.matrix[2][0] == 'equis' &&
+		this.matrix[1][1] == 'equis' &&
+		this.matrix[0][2] == 'equis')||
+		this.matrix[2][0] == 'circulo' &&
+		this.matrix[1][1] == 'circulo' &&
+		this.matrix[0][2] == 'circulo'
+		)
+	{
+		alert('gano');
+	}
+
+	
+
 }
 Matriz.prototype.insertarCambio = function(cambio, posicion) {
 	posicion = posicion.substring(5);
